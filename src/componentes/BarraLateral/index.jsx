f9f19867f91849c6114b8bf6618b1efdaa62f401
renawmontanari <regiangelica@gmail.com>
2024-06-10 18:54:37 -0300

    barra-lateral

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import ItemNavegacao from "./ItemNavegacao";
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,11 +13,20 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href="">
-                            Inicio
-                        </a>
-                    </li>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/home-ativo.png" 
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Inicio
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-vistas-ativo.png" 
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                        ativo={false}
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
