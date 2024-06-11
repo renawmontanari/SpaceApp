89381f1944537299f2533f9de8066fa4e67e7233
renawmontanari <regiangelica@gmail.com>
2024-06-11 19:19:10 -0300

    layout-desktop

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -27,6 +27,27 @@ const BarraLateral = () => {
                     >
                         Mais vistas
                     </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-curtidas-ativo.png" 
+                        iconeInativo="/icones/mais-curtidas-inativo.png"
+                        ativo={false}
+                    >
+                        Mais curtidas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/novas-ativo.png" 
+                        iconeInativo="/icones/novas-inativo.png"
+                        ativo={false}
+                    >
+                        Novas
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/surpreenda-me-ativo.png" 
+                        iconeInativo="/icones/surpreenda-me-inativo.png"
+                        ativo={false}
+                    >
+                        Surpreenda-me
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
