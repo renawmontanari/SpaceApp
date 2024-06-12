604c096655436d9b0db1e2332f895ddd08bb99c6
renawmontanari <regiangelica@gmail.com>
2024-06-12 19:58:28 -0300

    imagens

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import BarraLateral from "./componentes/BarraLateral";
 import Banner from "./componentes/Banner";
 import bannerBackground from "./assets/banner.png";
 import Galeria from "./componentes/Galeria";
+import { useState } from "react";
+
+import fotos from './fotos.json';
+
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -29,7 +33,9 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -42,7 +48,7 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!" 
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria} />
           </ConteudoGaleria>
         </Main>
       </AppContainer>
--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,22 +2,37 @@ import styled from "styled-components";
 import Titulo from "../Titulo";
 import Populares from "./Populares";
 import Tags from "./Tags";
+import Imagem from "./Imagem";
 
 const GaleriaContainer = styled.div`
     display: flex;
+    gap: 24px;
 `
 
 const SecaoFluida = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = () => {
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({ fotos = [] }) => {
     return (
         <>
             <Tags />
             <GaleriaContainer>
                 <SecaoFluida>
                     <Titulo>Navegue pela galeria</Titulo>
+                    <ImagensContainer>
+                        {fotos.map(foto => <Imagem 
+                            key={foto.id}
+                            foto={foto} />)
+                        }
+                    </ImagensContainer>
                 </SecaoFluida>
                 <Populares />
             </GaleriaContainer>
