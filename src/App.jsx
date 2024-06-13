37f6aa48db0766eecef8767d7761385bc5d502cb
renawmontanari <regiangelica@gmail.com>
2024-06-13 20:13:20 -0300

    opacidade-modal

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,7 @@ const ConteudoGaleria = styled.section`
 
 const App = () => {
   const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos);
+  const [fotoSelecionada, setFotoSelecionada] = useState(null); 
 
   return (
     <FundoGradiente>
@@ -50,11 +51,14 @@ const App = () => {
               texto="A galeria mais completa de fotos do espaço!" 
               backgroundImage={bannerBackground}
             />
-            <Galeria fotos={fotosDaGaleria} />
+            <Galeria 
+              aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+              fotos={fotosDaGaleria} 
+            />
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
-      <ModalZoom />
+      <ModalZoom foto={fotoSelecionada} />
     </FundoGradiente>
   )
 }
--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,12 +1,32 @@
-const ModalZoom = () => {
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+    background-color: rgba(0, 0, 0, 0.7);
+    position: fixed; 
+    top: 0; 
+    right: 0;
+    bottom: 0;
+    left: 0;
+`;
+
+const DialogEstilizado = styled.dialog`
+    position: absolute;
+    top: 294px;
+`;
+
+const ModalZoom = ({ foto }) => {
     return (
         <>
-            <dialog open>
-                <p>Greetings, one and all!</p>
+            {foto && <>
+                <Overlay />
+                <DialogEstilizado open={!!foto}>
+                <Imagem foto={foto} expandida={true} />
                 <form method="dialog">
                     <button>OK</button>
                 </form>
-            </dialog>
+                </DialogEstilizado>
+            </>}
         </>
     )
 }
