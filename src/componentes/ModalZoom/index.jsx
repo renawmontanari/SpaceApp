1e6965725ae90334537bdd5afa765faf8850c068
renawmontanari <regiangelica@gmail.com>
2024-06-14 19:04:17 -0300

    populares-modal

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Imagem from "../Galeria/Imagem";
+import BotaoIcone from "../BotaoIcone";
 
 const Overlay = styled.div`
     background-color: rgba(0, 0, 0, 0.7);
@@ -13,6 +14,19 @@ const Overlay = styled.div`
 const DialogEstilizado = styled.dialog`
     position: absolute;
     top: 294px;
+    background: transparent;
+    padding: 0;
+    border: 0;
+    width: 1156px;
+    display: flex;
+    justify-content: center;
+    form {
+        button {
+            position: relative;
+            top: 20px;
+            right: 60px;
+        }
+    }
 `;
 
 const ModalZoom = ({ foto }) => {
@@ -21,10 +35,12 @@ const ModalZoom = ({ foto }) => {
             {foto && <>
                 <Overlay />
                 <DialogEstilizado open={!!foto}>
-                <Imagem foto={foto} expandida={true} />
-                <form method="dialog">
-                    <button>OK</button>
-                </form>
+                    <Imagem foto={foto} expandida={true} />
+                    <form method="dialog">
+                        <BotaoIcone>
+                            <img src="/icones/fechar.png" alt="Icone de fechar" />
+                        </BotaoIcone>
+                    </form>
                 </DialogEstilizado>
             </>}
         </>
